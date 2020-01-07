a658de31f79b49ff1691457bd40869fbb123bfa7
jooyh <aguitarj@naver.com>
2020-01-07 12:27:51 +0900

    init settting for study.........

---
+++ main.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var template = require('./lib/template.js');
+
+// 서버 생성
+var app = http.createServer(function(request,response){
+    // 요청 url
+    var _url = request.url;
+    // 쿼리파라미터
+    var queryData = url.parse(_url, true).query;
+    // 경로
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/'){
+        // 쿼리파라미터가 없는경우
+        if(queryData.id === undefined) queryData.id = "main";
+
+        fs.readdir('./html', function(error, filelist){
+            fs.readFile(`./html/${queryData.id}.html`, 'utf8', function(err, content){
+                var html = template.getHtml(content);
+                response.writeHead(200);
+                response.end(html);
+            });
+        });
+
+    }
+});
+app.listen(3000);
