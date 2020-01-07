e09b5b3349581f23a4ee59b52d10af2158275fa1
yougmi <zoz7353@gmail.com>
2020-01-07 14:10:37 +0900

    누락파일 커밋

--- main.js
+++ main.js
@@ -8,20 +8,22 @@ var app = http.createServer(function(request,response){
     // 요청 url
     var _url = request.url;
     // 쿼리파라미터
-    var queryData = url.parse(_url, true).query;
+    // var queryData = url.parse(_url, true).query;
     // 경로
     var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
+    // if(pathname === '/'){
         // 쿼리파라미터가 없는경우
-        if(queryData.id === undefined) queryData.id = "main";
-        fs.readdir('./html', function(error, filelist){
-            fs.readFile(`./html/${queryData.id}.html`, 'utf8', function(err, content){
-                var html = template.getHtml(content);
-                response.writeHead(200);
-                response.end(html);
-            });
+        // if(queryData.id === undefined) queryData.id = "main";
+
+    if(pathname == "/") pathname = "main";
+    fs.readdir('./html', function(error, filelist){
+        fs.readFile(`./html/${pathname}.html`, 'utf8', function(err, content){
+            var html = template.getHtml(content);
+            response.writeHead(200);
+            response.end(html);
         });
-    }
+    });
+    // }
 });
 //서버 실행 (3000 포트)....
 app.listen(3000);
