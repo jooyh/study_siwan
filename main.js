1726c3c0365588aad3224c6230b8b758b963adad
jooyh <aguitarj@naver.com>
2020-01-08 13:50:04 +0900

    회원가입 로직 작성중

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/common/template.js');
+var join = require('./lib/common/join.js');
 
 //pm2 사용시 서버 실행 명령어 
 //pm2 start main.js --watch
@@ -26,22 +27,24 @@ var app = http.createServer(function(request,response){
         });
     // 그외 페이지 요청의 경우
     }else{
-        var test = '';
-        request.on('data', function(data){
-            console.log('start',data);
-            test += data;
-        });
-        request.on('end',function(){
-            var temp = null;
-            if(test){
-                temp = qs.parse(test);
-                console.log(temp);
-
-                fs.writeFile(`data/${temp.email.split("@")[0]}`,JSON.stringify(temp),"utf8",function(err){
-                    console.log(err);
-                });
-            }
-        });
+        if(pathname == "join"){
+            var test = '';
+            request.on('data', function(data){
+                console.log('start',data);
+                test += data;
+            });
+            request.on('end',function(){
+                var temp = null;
+                if(test){
+                    temp = qs.parse(test);
+                    console.log(temp);
+    
+                    fs.writeFile(`data/${temp.email.split("@")[0]}`,JSON.stringify(temp),"utf8",function(err){
+                        console.log(err);
+                    });
+                }
+            });
+        }
 
         fs.readFile(`./html/${pathname}.html`, 'utf8', function(err, content){
             var html = template.getHtml(content);
