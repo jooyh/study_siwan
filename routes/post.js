1a345a4d7b905ed5c073ca78da20a18f029c3c8d
jooyh <aguitarj@naver.com>
2020-01-16 11:48:14 +0900

    게시물 목록조회

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const fs = require('fs');
 const multer = require('multer');
 const path = require("path");
-const db = require('../lib/common/db.js');
+const connection = require('../lib/common/db.js');
+var async = require("async");
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -11,7 +12,7 @@ var storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         var extension = path.extname(file.originalname);
-        cb(null, file.fieldname + '-' + Date.now() + extension)
+        cb(null, Date.now() + extension)
     }
 });
 
@@ -19,14 +20,14 @@ const upload = multer({storage: storage, limits: { fileSize: 5 * 1024 * 1024 } }
 
 //파일등록
 router.post('/uploadpost.do',upload.array("atchFile"), function(req,res){
-    db.beginTransaction(function(err) {
+    connection.beginTransaction(function(err) {
         if (err) { 
             console.log('transaction err...');
             throw err; 
         }
         console.log("req.session",req.session);
         //게시물 등록
-        db.query(
+        connection.query(
             `insert into zoz7184.nb_post 
             (
                 post_content
@@ -51,14 +52,14 @@ router.post('/uploadpost.do',upload.array("atchFile"), function(req,res){
                 console.log("post.. insert",results);
                 if(err){
                     console.error(err);
-                    db.rollback(function(rollback_error) {
+                    connection.rollback(function(rollback_error) {
                         throw err;
                     });
                 } 
                 //게시물 아이디로 파일 등록
                 for(var i=0; i<this.req.files.length; i++){
                     var file = this.req.files[i];
-                    db.query(
+                    connection.query(
                         `insert into zoz7184.nb_atch_file 
                         (
                             post_id
@@ -91,14 +92,14 @@ router.post('/uploadpost.do',upload.array("atchFile"), function(req,res){
                         ,function(fErr,results,fields){
                             if(fErr){
                                 console.log("file insert err ..... ")
-                                db.rollback(function(rollback_error) {
+                                connection.rollback(function(rollback_error) {
                                     console.log("rollback");
                                     throw fErr;
                                 });
                             }
-                            db.commit(function(cmErr) {
+                            connection.commit(function(cmErr) {
                                 if (cmErr) {
-                                    db.rollback(function(rollback_error) {
+                                    connection.rollback(function(rollback_error) {
                                         throw cmErr;
                                     });
                                 }
@@ -112,8 +113,69 @@ router.post('/uploadpost.do',upload.array("atchFile"), function(req,res){
     });
 });
 
-router.post('/getpostlist.do', function(req,res){
-    db.query(
+
+
+router.post('/getpostlist.do', async function(req,res){
+    async.waterfall(
+        [function task1(callback) { 
+            connection.query(
+                `select post_id
+                       ,post_content
+                       ,post_hashtag
+                       ,reg_dtm
+                       ,reg_id
+                       ,upd_dtm
+                       ,upd_id
+                       ,(select user_name
+                        from zoz7184.nb_user u
+                       where p.reg_id = u.user_id) as user_name
+                   from zoz7184.nb_post p
+                  where reg_id in (?)
+                  order by upd_dtm desc
+                `
+                ,[req.session.user.user_id]
+                ,function(err,postResults,fields){
+                    if(err) throw err;
+                    console.log("task1",postResults);
+                    callback(null, postResults); 
+                }
+            );
+        }, function task2(arg, callback) {
+            async.forEachOfLimit(arg, 1, function(post, index, cb) {
+                console.log("TEST EACH "+index, arg[index]);
+                connection.query(
+                    `select post_id
+                            ,file_id
+                            ,file_snm
+                            ,file_onm
+                            ,file_path
+                       from zoz7184.nb_atch_file
+                      where post_id = ?
+                      order by upd_dtm desc
+                    `
+                    ,[arg[index].post_id]
+                    , function(err,fileResults,fields){
+                        if(err) throw err;
+                        arg[index].fileArr = fileResults;
+                        cb();
+                    }
+                )
+            }, function() {
+                console.log('ALL done')
+                callback(null, arg);
+            })
+        }]
+        , function (err, results) { 
+            console.log('task finish'); 
+            console.log(results); 
+            res.send(results);
+        } 
+    );
+});
+
+router.post('/getpostlist.do', async function(req,res){
+    var postList; 
+    await connection.query(
         `select post_id
                ,post_content
                ,post_hashtag
@@ -129,32 +191,38 @@ router.post('/getpostlist.do', function(req,res){
           order by upd_dtm desc
         `
         ,[req.session.user.user_id]
-        ,async function(err,postResults,fields){
-            // console.log("post result ",results)
+        ,await function(err,postResults,fields){
             if(err) throw err;
-            for(var i=0; i<postResults.length; i++){
-                db.query(
-                    `select post_id
-                            ,file_id
-                            ,file_snm
-                            ,file_onm
-                            ,file_path
-                       from zoz7184.nb_atch_file
-                      where post_id = ?
-                      order by upd_dtm desc
-                    `
-                    ,[postResults[i].post_id]
-                    ,await function(err,fileResults,fields){
-                        if(err) throw err;
-                        // console.log("test",fileResults);
-                        this.fileArr = fileResults;
-                    }.bind({post:postResults[i]})
-                )
-            }
-            // console.log("bf send...",results);
-            res.send(postResults);
+            postList = postResults;
         }
     );
+
+    for(var i=0; i<postList.length; i++){
+        await connection.query(
+            `select post_id
+                    ,file_id
+                    ,file_snm
+                    ,file_onm
+                    ,file_path
+               from zoz7184.nb_atch_file
+              where post_id = ?
+              order by upd_dtm desc
+            `
+            ,[postList[i].post_id]
+            ,await function(err,fileResults,fields){
+                if(err) throw err;
+                // console.log("test",fileResults);
+                postList[i].fileArr = fileResults;
+                console.log("Q2 result...")
+            }
+        )
+    }
+    console.log("for End...")
+
+
+
+    console.log("bf send...",results);
+    res.send(postResults);
 });
 
 router.post('/test.do',upload.array("atchFile"),function(req,res){
